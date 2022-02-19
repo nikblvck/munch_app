b9be23b327cf44a38d45361645846dacb54710d8
Nik <nikblvck@icloud.com>
2022-02-19 10:14:50 -0800

    updated seeder, likes appear on backend

---
+++ app/seeds/likes.py
@@ -0,0 +1,30 @@
+from app.models import db, Like
+
+def seed_likes():
+  likes = [
+    {
+      'user_id': 2,
+      'post_id': 1,
+    },
+    {
+      'user_id': 3,
+      'post_id': 1,
+    },
+    {
+      'user_id': 1,
+      'post_id': 3
+    },
+    {
+      'user_id': 1,
+      'post_id': 7
+    }
+  ]
+
+  for like in likes:
+    new_like = Like(**like)
+    db.session.add(new_like)
+  db.session.commit()
+
+def undo_likes():
+  db.session.execute('TRUNCATE likes RESTART IDENTITY CASCADE;')
+  db.session.commit()
