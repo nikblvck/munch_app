4017b42ffe29ec05fec17876f81e9f5301e92df6
Nik <nikblvck@icloud.com>
2022-02-23 14:35:51 -0800

    fixed backend error

--- app/forms/new_post.py
+++ app/forms/new_post.py
@@ -3,7 +3,7 @@ from wtforms import StringField, IntegerField, TextAreaField, SubmitField
 from wtforms.validators import DataRequired, Length
 
 class NewPost(FlaskForm):
-    image_url=StringField('Image URL', validators=[DataRequired()], message='Please provide an image URL for your post.')
+    image_url=StringField('Image URL', validators=[DataRequired()])
     caption=StringField('Caption')
-    category_id = IntegerField('Category ID', validators = [DataRequired()], message='Please provide a category name for your post.')
+    category_id = IntegerField('Category ID', validators = [DataRequired()])
     submit = SubmitField('Post')
