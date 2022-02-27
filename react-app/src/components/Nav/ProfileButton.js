9cacd86ad09ba6d48e5151f233b43009f9c7f429
Nik <nikblvck@icloud.com>
2022-02-27 12:39:32 -0800

    user dropdown complete

--- react-app/src/components/Nav/ProfileButton.js
+++ react-app/src/components/Nav/ProfileButton.js
@@ -52,14 +52,25 @@ function ProfileButton({user}) {
             <div className="profile_image_div">
               <img className="profile_img" src={user.profile_img_url} alt="profile_image"/>
             </div>
+            <div className="profile_name">
+              <div><h1 className="bold_text" id="dropdown_username">{user.first_name} {user.last_name}</h1></div>
+            <div className="subtext">Member Since
+              <p className="bold_text">{user.member_since}</p>
+            </div>
+            <div className="post_count">
+              {user.post_count} Posts
+            </div>
+            </div>
               <div className="profile_d_links">
-                <button onClick={handleClick} className="nav_link">
+                <button onClick={handleClick} className="profile_link">
                   <i className="fa-solid fa-plus"></i>
+                   <p className="nav_link_profile">New Post</p>
                 </button>
               </div>
               <div className="profile_d_links">
-                <button onClick={handleLogout} className="nav_link">
+                <button onClick={handleLogout} className="profile_link">
                   <i className="fa-solid fa-sign-out-alt"></i>
+                 <p className="nav_link_profile">Logout</p>
                 </button>
                 </div>
             </div>
