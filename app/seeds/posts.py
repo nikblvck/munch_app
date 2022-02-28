2e0caa573db369310746d6358e2e5cf4ac58bd34
Nik <nikblvck@icloud.com>
2022-02-27 22:03:54 -0800

    update seeders:

--- app/seeds/posts.py
+++ app/seeds/posts.py
@@ -3,10 +3,23 @@ from app.models import db, Post
 def seed_posts():
   posts = [
     {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645954902/munch/vegetarian%20burger.jpg',
+        'caption': 'Vegetarian Burger',
+        'user_id': 1,
+        'category_id': 3
+    },
+    {
      'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1644967654/munch/image_yupy3k.jpg',
      'caption': 'This is shrimp & sausage gumbo served with a side of white rice. 👌🏾 Yummy! 🍜',
      'category_id': 19,
      'user_id': 1
+    },
+    {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645954699/munch/vegan_pineapple_pizza.png',
+        'caption': 'This is a vegan pizza with pineapple and cheese. 🍕',
+        'user_id': 10,
+        'category_id': 2
+
     },
     {
       'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1644967582/munch/image_oeia0k.jpg',
@@ -21,10 +34,22 @@ def seed_posts():
       'category_id': 16
     },
     {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645954784/munch/vegan_air_fried_tofu.jpg',
+        'caption': 'This is a vegan air fried tofu with a side of white rice. 🍜',
+        'user_id': 2,
+        'category_id': 2
+    },
+    {
       'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1644967680/munch/image_coe8bg.jpg',
       'caption': 'Grillin some steak',
       'user_id': 1,
       'category_id': 4
+    },
+    {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645954872/munch/vegetarian_breakfast_nachos.jpg',
+        'caption': 'Veg Brek Nachos',
+        'user_id': 9,
+        'category_id': 3
     },
     {
       'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1644970170/munch/image_uomerr.jpg',
@@ -44,6 +69,12 @@ def seed_posts():
       'category_id': 4
     },
     {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645954954/munch/vegetarian_bbq.jpg',
+        'caption': 'BBQ MY WAY!',
+        'user_id': 7,
+        'category_id': 3
+    },
+    {
       'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645480014/munch/image_xj5zs0.jpg',
       'caption': 'Lobster tails anyone? First time making them and it definitely will not be my last! Not pictured: garlic mashed potatoes, roasted asparagus and cheddar bay biscuits.',
       'user_id': 1,
@@ -60,7 +91,43 @@ def seed_posts():
       'caption': 'Tried something new - made queso on the grill, BOMB.COM okay?! ',
       'user_id': 1,
       'category_id': 5
-    }
+    },
+     {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645952488/munch/soulfood06.jpg',
+        'caption': 'Tried out my grandma\'s famous gumbo recipe! I think I will go a bit lighter on the roux next time!',
+        'user_id': 1,
+        'category_id':1
+    },
+    {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645952538/munch/soulfood07.jpg',
+        'caption': 'Could not wait for the holidays to roll around again so I made my own Thanksgiving in February!',
+        'user_id': 7,
+        'category_id':1
+    },
+    {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645952654/munch/soulfood08.jpg',
+        'caption': 'Chicken, macaroni and cheese with some black eyed peas. Delicious!',
+        'user_id': 8,
+        'category_id':1
+    },
+    {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645952448/munch/soulfood03.jpg',
+        'user_id': 1,
+        'category_id':1
+    },
+    {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645954602/munch/vegan_mushroom_blt.jpg',
+        'caption': 'Mushroom "BLT"',
+        'user_id': 9,
+        'category_id': 2
+    },
+    {
+        'image_url': 'https://res.cloudinary.com/bigtechnik/image/upload/v1645954566/munch/vegan_lemon_bars.png',
+        'user_id': 9,
+        'category_id': 2
+    },
+
+
   ]
 
   for post in posts:
